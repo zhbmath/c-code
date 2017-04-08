f4645e811797e2fad8cbd6e352e335c56251c62f
zhbmath <zhbmath@163.com>
2017-04-08 12:19:16 +0800

    Update b.c
    format  the file 
    
    change the order of  a, b, c

--- b.c
+++ b.c
@@ -1,10 +1,10 @@
-#include "stdio.h"
+#include <stdio.h>
 
 static int (*p)(int, int);
 
 static int add(int a, int b)
 {
-     return a+b ;
+    return a+b ;
 }
 
 static int sub(int a, int b)
@@ -12,7 +12,7 @@ static int sub(int a, int b)
     return a-b ;
 }
 
-int result( char c, int a, int b )
+int result( int a, int b, char c )
 {
     if ( c == '+' )  p = add;
     if ( c == '-' )  p = sub;
