5faab6943cdce0f929b575f0a8d354f4f8d347d2
zhbmath <zhbmath@163.com>
2017-12-21 14:49:25 +0800

    format

--- node/node.c
+++ node/node.c
@@ -8,31 +8,31 @@ typedef struct node{
 
 int main(void)
 {
-  int i=0;
-  NODE *head=NULL, *pf, *pb;
+    int i=0;
+    NODE *head=NULL, *pf, *pb;
 
-  for(i=1; i<=3; i++)
-  {
-      pb=(NODE*) malloc(sizeof(NODE));
+    for(i=1; i<=3; i++)
+    {
+        pb=(NODE*) malloc(sizeof(NODE));
 
-      if(i==1) 
-      {
-          pf=head=pb;
-      }
-      else
-      {
-          pf->next=pb;
-          pf->id=i;
-          pb->next=NULL;
-          pf=pb;
-      }
-  }
+        if(i==1) 
+        {
+            pf=head=pb;
+        }
+        else
+        {
+            pf->next=pb;
+            pf->id=i;
+            pb->next=NULL;
+            pf=pb;
+        }
+    }
 
-  while(head->next != NULL)
-  {
-    head++;
-    printf("%d\n", head->id);
-  }
+    while(head->next != NULL)
+    {
+      head++;
+      printf("%d\n", head->id);
+    }
 
-  return 0;
+    return 0;
 }
