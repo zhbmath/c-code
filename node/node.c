feb9b32521d821f9299e2c52cb76bd2a69a36180
huibo.zhao <huibo.zhao@sihuatech.com>
2019-06-11 18:07:45 +0800

    test node in a func

--- node/node.c
+++ node/node.c
@@ -1,38 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 typedef struct node{
   int data;
   struct node *next;
 }NODE;
 
-int main(void)
+static int create_node(NODE** head);
+
+int create_node(NODE** head)
 {
     int i=0;
-    NODE *head=NULL, *pf, *pb;
+    NODE  *pf, *pb;
 
     for(i=0; i<=5; i++)
     {
-        pb=(NODE*) malloc(sizeof(NODE));
+      pb = (NODE*)malloc(sizeof(NODE));
 
-        if(i==1) 
-        {
-            pf=head=pb;
-        }
-        else
-        {
-            pf->next=pb;
-            pf->data=i;
-            pb->next=NULL;
-            pf=pb;
-        }
+      if (NULL==pb)
+      {
+          exit(1);
+          printf("allocate failed");
+      }
+
+      memset(pb, 0, sizeof(NODE));
+
+      if(i==0)
+      {
+          pf=*head=pb;
+          pb->data=i;
+          pb->next=NULL;
+      }
+      else
+      {
+          pf->next=pb;
+          pf->data=i;
+          pb->next=NULL;
+          pf=pb;
+      }
     }
+
+    return 0;
+}
+
+int main(void)
+{
+    NODE *head=NULL;
+    create_node(&head);
 
     while(head->next != NULL)
     {
       printf("%d\n", head->data);
       head=head->next;
     }
-
-    return 0;
 }
